1fda67052f59cf58a4ccba1d3d371ef0d412676b
khanh <dinhkhanh1881@gmail.com>
2025-12-07 21:53:27 +0700

    feat/khanh-update

--- main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.sidebar-btn');
-const navMenu = document.querySelector('nav');
-
-menuBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
