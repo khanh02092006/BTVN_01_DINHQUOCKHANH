e65b8fcbc2759dc9c0fb20e0aa38cee112751852
khanh <dinhkhanh1881@gmail.com>
2025-12-14 15:58:28 +0700

    baitapjs

---
+++ baitapjs/baitap.js
@@ -0,0 +1,33 @@
+//2626
+// bài 1
+// let khanh = (nums, fn, init) => {
+//     let accum = init;
+//     nums.forEach((num) => {
+//         accum = fn(accum, num);
+//     });
+//     return accum;
+// }
+// nums = [1, 2, 3, 4];
+// console.log(khanh(nums, (a, b) => a + b, 0));
+// bai 2
+// let khanh = (nums, fn, init) => {
+//     let accum = init;
+//     nums.forEach((num) => {
+//         accum = fn(accum, num);
+//     });
+//     return accum;
+// }
+// nums = [1, 2, 3, 4];
+// console.log(khanh(nums, (a, b) => a + (b * b), 100));
+// bai3
+// let khanh = (nums, fn, init) => {
+//     let accum = init;
+
+//     nums.forEach((num) => {
+//         accum = fn(accum, num);
+//     });
+
+//     return accum;
+// };
+// nums = [];
+// console.log(khanh(nums, (a, b) => a + b, 25));
