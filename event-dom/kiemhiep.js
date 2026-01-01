ae6c9d1e5e2c9600ddc47b4f5b808aad98d4db73
khanh <dinhkhanh1881@gmail.com>
2026-01-01 21:47:45 +0700

    fixlan2

--- event-dom/kiemhiep.js
+++ event-dom/kiemhiep.js
@@ -1,16 +1,9 @@
 const container = document.querySelector(".container");
 
-async function fetchCharacters() {
-    const res = await fetch("https://zelda.fanapis.com/api/characters?limit=10");
-    const data = await res.json();
-    const list = data.data;
-
-    const first = list[0];
-
-    const sidebarHTML = (list) => {
-        let HTML = "";
-        list.forEach((item, index) => {
-            HTML += `
+const sidebarHTML = (list) => {
+    let HTML = "";
+    list.forEach((item, index) => {
+        HTML += `
       <div class="story-item">
         <div class="rank-number">${index + 1}</div>
         <div class="story-info">
@@ -19,12 +12,12 @@ async function fetchCharacters() {
         </div>
       </div>
     `;
-        });
-        return HTML;
-    };
+    });
+    return HTML;
+};
 
-    const renderLayout = (first, sidebarHTML) => {
-        container.innerHTML = `
+const renderLayout = (first, sidebarHtmlString) => {
+    container.innerHTML = `
     <div class="left-sidebar">
       <img src="https://genk.mediacdn.vn/k:2016/1-1465352650525/5-ly-do-khien-soai-ca-kiem-hiep-duoc-van-nguoi-me.jpg"
         alt="Vạn Cổ Đệ Nhất Thần" class="book-cover">
@@ -41,45 +34,26 @@ async function fetchCharacters() {
       <div class="story-intro">${first.description}</div>
       <div class="section-title">Giới thiệu nội dung:</div>
 
-            <div class="dialogue">
-                "Diễn nhì, Tiểu Thiên Nhất, bên này."
-            </div>
-
-            <div class="story-description">
-                <p style="margin-bottom: 15px;">An Tuyệt Thiên như là hòa tan đông tuyết, kêu vô cùng thân mật, còn
-                    ngoặc.</p>
-
-                <div class="dialogue">
-                    "'Cất. Không biết xấu hổ." Ngụy Ôn Lan trợn mắt một cái, thầm thầm mắng một câu.
-                </div>
-
-                <p style="margin-bottom: 15px;">"Đồng cảm." An Ninh cũng nói.</p>
-
-                <p style="margin-bottom: 15px;">Tựa hồ tại chặn ghét hai nữ nhân này phương diện, mẹ con các nàng lại đã
-                    đạt thành nhất trí.</p>
-
-                <p style="margin-bottom: 15px;">Lâm Mộc Đông Diễn cũng An Thiên Nhất đến lúc, tại chỗ 3000 bộ lạc dự
-                    tiệc người, cơ hồ đều ngưng tư minh nói chuyện với nhau, mắt lộ ra vẻ sùng kính, nhìn về phía cái
-                    này quy phục cũng qúy tứ.</p>
-
-                <div class="dialogue">
-                    "Cô cổ." Mộc Đông Diễn ôn nhu mỉm cười, thanh âm rất để nghe, cũng làm cho rất thân mật, mang theo
-                    thiếu niên kia An Thiên Nhất, leo lên Tuyệt Tỉnh hào.
-                </div>
-
-                <p>"Thiên Nhất." An Sương, An Huyền Minh, An Như Yên chờ Cổ bảng thiên tài, đều hướng thiếu niên tóc
-                    vàng kia gật đầu.</p>
-            </div>
+      <div class="dialogue">
+        "Diễn nhì, Tiểu Thiên Nhất, bên này."
+      </div>
     </div>
 
     <div class="right-sidebar">
       <div class="sidebar-title">TOP Truyện Đề Cử</div>
-      ${sidebarHTML}
+      ${sidebarHtmlString}
     </div>
   `;
-    };
+};
 
+async function fetchCharacters() {
+    const res = await fetch("https://zelda.fanapis.com/api/characters?limit=10");
+    const data = await res.json();
+    const list = data.data;
+
+    const first = list[0];
     const sidebar = sidebarHTML(list.slice(1));
+
     renderLayout(first, sidebar);
 }
 
