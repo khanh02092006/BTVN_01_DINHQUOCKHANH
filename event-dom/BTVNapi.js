eda7330993a6d4a615dc188b6c7a6fede5cbc9ec
khanh <dinhkhanh1881@gmail.com>
2026-01-10 15:05:55 +0700

    fix2

--- event-dom/BTVNapi.js
+++ event-dom/BTVNapi.js
@@ -1,11 +1,19 @@
 const addusser = document.querySelector('.btn-primary');
 const userGrid = document.querySelector('.user-grid');
-
+const modal = document.getElementById("addModal");
+const addForm = document.getElementById("addForm");
+const closeModal = document.getElementById("closeModal");
+const nameinput = document.getElementById("name");
+const username = document.getElementById("username");
+const ip1 = document.getElementById("ip1");
+const ip2 = document.getElementById("ip2");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const staticData = document.getElementById("staticData");
+const key_username = "USERNAMEUSER";
 const featchUser = async () => {
     const res = await fetch("https://69606493e7aa517cb795f700.mockapi.io/users?limit=12");
     res.json().then((data) => {
-        console.log("data11111", data);
-        console.log(data.length)
 
         userGrid.innerHTML = "";
 
@@ -30,46 +38,36 @@ const featchUser = async () => {
         });
     });
 };
-userGrid.addEventListener("click", (e) => {
+let editUserId = null;
+
+userGrid.addEventListener("click", async (e) => {
     const card = e.target.closest(".card");
-    if (!card) {
-        return
-    };
-    if (e.target.closest(".btn-danger") || e.target.closest(".btn-success")) return;
-
-    const cardId = card.id;
-    if (!cardId) return;
-
-    window.location.href = `profile.html?id=${cardId}`;
-});
-userGrid.addEventListener("click", (e) => {
-    const deletebtn = e.target.closest(".btn-danger");
-    if (!deletebtn) return;
-
-
-    const card = deletebtn.closest(".card");
     if (!card) return;
+    const id = card.id;
 
-    const cardId = card.id;
-    if (!cardId) return;
 
-    const featchdelete = async (id) => {
-        await fetch(`https://69606493e7aa517cb795f700.mockapi.io/users/:${id}`, {
-            method: "DELETE",
-        });
+    if (e.target.closest(".btn-danger")) {
+        await featchdelete(id, card);
+        return;
+    }
 
-        card.remove();
-        console.log("card", card)
-    };
 
-    featchdelete(cardId);
+
+
+    window.location.href = `profile.html?id=${id}`;
 });
 
+const featchdelete = async (id, card) => {
+    await fetch(`https://69606493e7aa517cb795f700.mockapi.io/users/${id}`, {
+        method: "DELETE",
+    });
+
+    card.remove();
+
+};
 
 featchUser();
-const modal = document.getElementById("addModal");
-const addForm = document.getElementById("addForm");
-const closeModal = document.getElementById("closeModal");
+
 
 
 addusser.addEventListener("click", () => {
@@ -84,14 +82,7 @@ closeModal.addEventListener("click", () => {
 
 
 
-const nameinput = document.getElementById("name");
-const username = document.getElementById("username");
-const ip1 = document.getElementById("ip1");
-const ip2 = document.getElementById("ip2");
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const staticData = document.getElementById("staticData");
-const key_username = "USERNAMEUSER";
+
 
 const setLocalStoreage = (key, value) => {
     localStorage.setItem(key, value);
@@ -100,7 +91,7 @@ const setLocalStoreage = (key, value) => {
 const getLocalStoreage = (key) => {
     localStorage.getItem(key);
 };
-addForm.addEventListener("submit", (e) => {
+addForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const payload = {
         name: nameinput.value,
@@ -115,13 +106,32 @@ addForm.addEventListener("submit", (e) => {
             staticData: staticData.value.split(",").map(Number),
         },
     };
-    const featchAddUser = async (data) => {
-        await fetch("https://69606493e7aa517cb795f700.mockapi.io/users", {
-            method: "POST",
+
+    await featchAddUser(payload);
+    alert(" Thêm user thành công!");
+
+    modal.style.display = "none";
+    featchUser();
+});
+const featchAddUser = async (data) => {
+    await fetch("https://69606493e7aa517cb795f700.mockapi.io/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+
+};
+
+const featchUpdateUser = async (id, data) => {
+    const res = await fetch(
+        `https://69606493e7aa517cb795f700.mockapi.io/users/${id}`,
+        {
+            method: "PUT",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data),
-        });
+        }
+    );
 
-    };
-    featchAddUser(payload);
-});
+    return res.json();
+};
+
